7f29905531e399f1c5b14798bd3e883a078dffa4
Kevin St.Onge <kevinstonge@gmail.com>
2020-06-14 21:38:55 -0400

    closure.js complete

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,19 @@ myFunction();
 
 // Explanation: 
 
+/*
+    functions will first look for variables/methods inside of their own block (enclosed by the curly braces), if the information is not found there, they will then look up one level of scope (inside the curly braces that surround the function itself). However, they cannot see inside of neighboring functions, closure/scope is a one-way visibility pattern.
+*/
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+const summation=(n)=>{
+  let sum = 0;
+  while (n>0) {
+      sum += n--;
+  }
+  return sum;
+}
+console.log(summation(4))
